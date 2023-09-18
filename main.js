5d2f6cf662e3e610d29139d098db7f52fff1c6ed
Simone Mongelli <mongelli.simone.2004@gmail.com>
2023-09-18 18:19:59 +0200

    esercizi array e funzioni

---
+++ main.js
@@ -0,0 +1,71 @@
+// ESERCIZIO 1 FUNZIONI
+// function punteggio(number) {
+//     let punteggio1 = 0;
+//     let punteggio2 = 0;
+//     for (let i = 0; i <number ; i++) {
+//         let random1 = Math.floor(Math.random() * (6 - 1  + 1) + 1);
+//         let random2 = Math.floor(Math.random() * (6 - 1  + 1) + 1);
+//         punteggio1 = punteggio1 + random1;
+//         punteggio2 = punteggio2 + random2;
+//     }
+//     if (punteggio1> punteggio2){
+//         console.log(`Il vincitore è giocatore1 `);
+//     } else if (punteggio1 < punteggio2) {
+//         console.log(`Il vincitore è giocatore2`);
+//     }else {
+//         console.log(`Pareggio`);
+//     }
+// }
+// punteggio(5)
+
+// ESERCIZIO 2 MULTIPLI
+// function multipli(N) {
+//     for (let i = 0; i <= N; i++) {
+//     if(i % 3 ==0){
+//     console.log(`Fizz`);
+//   } else if (i % 5 ==0){
+//     console.log(`Buzz`);
+//   } else if (i % 15 ==0){
+//     console.log(`FizzBuzz`);
+//   } else {
+//     console.log(i);
+//   }  
+// }
+
+// }
+// multipli(15)
+
+// ESERCIZIO 3 
+
+// let numbers = [10, 12, 78, -4, -20, 11]
+
+// function Nmaggiore() {
+//     Math.max(numbers)
+//     Math.min(numbers)
+//     return undefined;
+
+    
+// }
+
+
+// let numbers = [10, 12, 78, -4, -20, 11];
+
+
+// function array1 (NN) {
+ 
+//   let max = Math.max(...NN);
+  
+//   console.log(`Numero maggiore: ${max}`);
+// }
+
+
+// function array2(NN) {
+ 
+//   let min = Math.min(...NN);
+//   console.log(`Numero maggiore: ${min}`);
+// }
+
+
+// array1(numbers)
+// array2(numbers)
+
